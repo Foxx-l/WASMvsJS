4ea731c3fed4f868769b76ba2c26fbeef9bdd4ea
Foxx-l <102874107+Foxx-l@users.noreply.github.com>
2024-02-26 21:18:04 +0100

    so you dont have to build

---
+++ pkg/WASMvsJS.d.ts
@@ -0,0 +1,50 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {number} limit
+* @returns {Uint32Array}
+*/
+export function wasm_sieve(limit: number): Uint32Array;
+/**
+* @param {number} size
+*/
+export function wasm_ram_test(size: number): void;
+/**
+* @param {number} count
+*/
+export function wasm_create_elements(count: number): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly wasm_sieve: (a: number, b: number) => void;
+  readonly wasm_ram_test: (a: number) => void;
+  readonly wasm_create_elements: (a: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
